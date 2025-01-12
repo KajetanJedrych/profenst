2aaa08861ed28055dba265f0a6ae85eb2f6f5c2e
KajetanJedrych <jedrychk@gmail.com>
2025-01-12 21:48:34 +0100

    add google analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Services from './components/Services';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
+import ReactGA from 'react-ga';
+
+// Inicjalizacja Google Analytics z Twoim ID śledzenia
+ReactGA.initialize('G-2GRZY4Q1FJ'); // Zamień na swoje ID śledzenia
+
+// Śledzenie każdej wizyty na stronie głównej
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => (
   <div>
